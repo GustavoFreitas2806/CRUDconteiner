dd99536bc9028386dee98cc7ecfebb313398fd8d
Gustavo Freitas Albuquerque <gustavofreita2013@gmail.com>
2023-03-16 17:22:53 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>T2S</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="titulo">
+            <h1 class="text-title">Container</h1>
+        </div>
+        <div class="container-dados">
+            <label class="label-dados "for="name">Digite o número do container aqui</label>
+            <input class="input-dados" type="text" name="numero-container" id="numero-container" placeholder="Digite o número de container aqui">
+            <label class="label-dados for="name">Digite o número do container aqui</label>
+            <input class="input-dados" type="text" name="numero-container" id="numero-container" placeholder="Digite o número de container aqui">
+            <label class="label-dados for="name">Digite o número do container aqui</label>
+            <input class="input-dados" type="text" name="numero-container" id="numero-container" placeholder="Digite o número de container aqui">
+            <label class="label-dados for="name">Digite o número do container aqui</label>
+            <input class="input-dados" type="text" name="numero-container" id="numero-container" placeholder="Digite o número de container aqui">
+            <button class="botao-add">Adicionar</button>
+        </div>
+    </div>
+
+    
+</body>
+</html>
